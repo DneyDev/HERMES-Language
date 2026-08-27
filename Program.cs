1330906f278e198c6b43620dcc7cbdd4469994d6
DneyDev <contdneydev@gmail.com>
2026-08-27 13:59:10 -0300

    test: atualiza programa de teste do parser

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 using HERMESLANG.Lexer;
+using HERMESLANG.Parser;
 
-class Program
-{
-    static void Main()
-    {
-        string codigo = """
+string codigo = """
 vida = 100
 
 se vida > 50:
     mostrar("Vida alta")
 """;
 
-        Lexer lexer = new Lexer(codigo);
+Lexer lexer = new Lexer(codigo);
 
-        List<Token> tokens = lexer.ScanTokens();
+List<Token> tokens = lexer.ScanTokens();
 
-        foreach (Token token in tokens)
-        {
-            Console.WriteLine(token);
-        }
-    }
+Parser parser = new Parser(tokens);
+
+try
+{
+    List<Statement> statements = parser.Parse();
+
+    Console.WriteLine("Parser executado com sucesso!");
+    Console.WriteLine($"Statements encontrados: {statements.Count}");
+}
+catch (ParserException ex)
+{
+    Console.WriteLine($"Erro de Parser: {ex.Message}");
 }
