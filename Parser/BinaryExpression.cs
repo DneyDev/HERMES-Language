593d911e91eb33c921c41e60ffcf809d2641f53a
DneyDev <contdneydev@gmail.com>
2026-08-27 16:38:15 -0300

    fix: ajuste de unidade do Parser

--- Parser/BinaryExpression.cs
+++ Parser/BinaryExpression.cs
@@ -5,7 +5,6 @@ public class BinaryExpression : Expression
     public Expression Left { get; }
     public HERMESLANG.Lexer.Token Operator { get; }
     public Expression Right { get; }
-
     public BinaryExpression(
         Expression left,
         HERMESLANG.Lexer.Token @operator,
